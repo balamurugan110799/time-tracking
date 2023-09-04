602d8bd2838040eff1b5040e09e1063f7c9bc890
balamurugan110799 <balamurugan99711@gmail.com>
2023-09-04 08:56:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,170 @@
-import logo from './logo.svg';
 import './App.css';
+import jeremy from "./components/images/image-jeremy.png"
+import work from "./components/images/icon-work.svg"
 
 function App() {
+  const data = [
+    {
+      "title": "Work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "Play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "Study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "Exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "Social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "Self Care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+  ]
+
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-[100%] bg-very-dark-blue p-[50px]">
+      <div className=' grid grid-cols-4 gap-4 h-[100%]'>
+        <div className='bg-[#1d204b] h-[100%] rounded-[20px]'>
+          <div className=' bg-[#5746ea] h-[70%] rounded-[20px] p-10 '>
+
+            <div className=' '>
+                <img src={jeremy} alt="jeremy" className=' h-[100px] w-[100px] border-2 border-[#fff] rounded-[50%]'/>
+            </div>
+
+            <p className=' text-[#9d98f8] pt-10'>Report for</p>
+
+            <h1 className='text-h1 text-[#fff] leading-[46px]'>Jeremy</h1>
+            <h1 className='text-h1 text-[#fff] leading-[46px]'>Robson</h1>
+
+          </div >
+
+          <div className='px-10 py-3'>
+          <div className=' hover:text-[#fff] duration-300 cursor-pointer text-desaturated-blue pb-1'>
+            Daily
+          </div>
+          <div className='hover:text-[#fff] duration-300  cursor-pointer text-desaturated-blue pb-1'>
+            Weekly
+          </div>
+          <div className='hover:text-[#fff] duration-300  cursor-pointer text-desaturated-blue pb-1'>
+            Monthly
+          </div>
+          </div>
+       
+
+
+        </div>
+        <div className=' col-span-3 h-[100%]'>
+          <div className=' grid grid-cols-3 gap-10 h-[100%] '>
+            <div className='relative'>
+            <div className=' bg-secondary rounded-[20px] h-[46%] '>
+            <div className=' h-[20%] z-10 z-index-low '>
+              <img src={work} alt="work" className='z-10 z-index-low'/>
+            </div>
+          <div className=' bg-[#1d204b] z-20 h-[80%] rounded-[20px] z-index-high '>
+
+          </div>
+            </div>
+          
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+       
+
+
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: { min: "0px", max: "640px" }, // mobile view 
+      smmd: { min: "641px", max: "767px" }, // small tablets
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "768px", max: "1023px" }, // ipad view 
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "1024px", max: "1279px" }, // laptop
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1280px", max: "1440px" }, //  desktop
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1441px", max: "1536px" }, // large desktop
+      // => @media (min-width: 1536px) { ... }
+      "3xl": "1537  px", // 4k screen 
+    },
+    boxShadow: {
+      ctr: "12px 12px 12px 1px rgb(0 0 0 / 0.1)",
+    },
+    fontSize: {
+      sm: "12px",
+      tiny: "13px",
+      base: "14px",
+      title: "13.5px",
+      h6: "16px",
+      h5: "18px",
+      h4: "20px",
+      h3: "22px",
+      h2: "24px",
+      h1: "36px",
+    },
+    colors: {
+      'primary': 'hsl(246, 80%, 60%)',
+      'secondary': 'hsl(15, 100%, 70%)',
+      'soft-blue': 'hsl(195, 74%, 62%)',
+      'light-red': 'hsl(348, 100%, 68%)',
+      "lime-green ": "hsl(145, 58%, 55%)",
+      "violet": "hsl(264, 64%, 52%)",
+      "soft-orange": "hsl(43, 84%, 65%)",
+      'very-dark-blue': 'hsl(226, 43%, 10%)',
+      'desaturated-blue': 'hsl(235, 45%, 61%)',
+      'pale-blue': 'hsl(236, 100%, 87%)',
+    },
+
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
